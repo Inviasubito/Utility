542d35203156bdcd95787d3cc0b0f0eaffd7f4c1
TizioBj <Inviasubito@users.noreply.github.com>
2023-03-31 12:02:41 +0200

    Creazione documento xml
    Il metodo Build utilizza l'oggetto XmlDocument per creare l'intera struttura XML e salvarla in un file.
    Il metodo BuildNoMemory utilizza l'oggetto XmlWriter per scrivere il documento XML direttamente nel file.

---
+++ Unity/XmlBuilder.cs
@@ -0,0 +1,60 @@
+using System.Xml;
+
+public class XmlBuilder : MonoBehaviour
+{
+    private void Build(string path)
+    {
+        XmlDocument xmlDoc = new XmlDocument();
+
+        // Creazione dell'elemento radice
+        XmlElement root = xmlDoc.CreateElement("root");
+        xmlDoc.AppendChild(root);
+
+        // Creazione di un nuovo elemento
+        XmlElement newElement = xmlDoc.CreateElement("block");
+        newElement.SetAttribute("active", "true");
+        newElement.SetAttribute("name", "A");
+        newElement.SetAttribute("child", "true");
+
+        // Creazione di un elemento figlio
+        XmlElement childElement = xmlDoc.CreateElement("elem");
+        childElement.SetAttribute("name", "B");
+        childElement.SetAttribute("mode", "both");
+        childElement.SetAttribute("full", "true");
+
+        // Aggiungi l'elemento figlio al nuovo elemento
+        newElement.AppendChild(childElement);
+
+        // Aggiungi il nuovo elemento al file XML
+        root.AppendChild(newElement);
+
+        // Salva il file XML
+        xmlDoc.Save(path);
+    }
+    
+    //Non crea la struttura in memoria ma scrive direttamente sul file (più performante)
+    private void BuildNoMemory(string path)
+    {
+        // Creazione dell'oggetto XmlWriter
+        XmlWriterSettings settings = new XmlWriterSettings();
+        settings.Indent = true;
+        XmlWriter xmlWriter = XmlWriter.Create(path, settings);
+
+        // Scrittura del documento XML
+        xmlWriter.WriteStartElement("root");
+        xmlWriter.WriteStartElement("block");
+        xmlWriter.WriteAttributeString("active", "true");
+        xmlWriter.WriteAttributeString("name", "A");
+        xmlWriter.WriteAttributeString("child", "true");
+        xmlWriter.WriteStartElement("elem");
+        xmlWriter.WriteAttributeString("name", "B");
+        xmlWriter.WriteAttributeString("mode", "both");
+        xmlWriter.WriteAttributeString("full", "true");
+        xmlWriter.WriteEndElement(); // chiusura di elem
+        xmlWriter.WriteEndElement(); // chiusura di block
+        xmlWriter.WriteEndElement(); // chiusura di root
+
+        // Chiusura dell'oggetto XmlWriter e salvataggio del file XML
+        xmlWriter.Close();
+    }
+}
